b1dff204be893f2115ee4611cfcba3a31f67b368
NjauSamuel <njaus602@gmail.com>
2024-09-04 21:49:52 +0300

    Made some nice UI changes.

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,30 +1,35 @@
 <template>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
         <Box v-for="listing in listings" :key="listing.id">
+
             <div>
-                <Link :href="route('listing.show', {listing: listing.id})">
-                    {{ listing.street }} - {{ listing.street_nr }} - {{ listing.city }}, for {{ listing.price }}
+                <Link :href="route('listing.show', {listing: listing.id})" >
+                    <ListingAddress :listing="listing" />
                 </Link>
             </div>
-        
+
             <div>
-                <Link :href="route('listing.edit', listing.id)">Edit</Link>
+                <Link :href="route('listing.edit', {listing: listing.id})" >
+                    Edit
+                </Link>
             </div>
 
             <div>
-                <Link :href="route('listing.destroy', listing.id)" method="Delete" as="button">Delete</Link>
+                <Link :href="route('listing.destroy', {listing: listing.id})" method="DELETE" as="button" >
+                    Delete
+                </Link>
             </div>
+      </Box>
 
-        </Box>
     </div>
-</template>
-
-<script setup>
-
+  </template>
+  
+  <script setup>
     import {Link} from '@inertiajs/vue3'
+    import ListingAddress from '@/Components/ListingAddress.vue'
     import Box from '@/Components/UI/Box.vue'
 
     defineProps({
         listings: Array, 
     })
-</script>
+  </script>
