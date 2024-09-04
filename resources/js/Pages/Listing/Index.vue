be193b45add86dc8373822b4002270dc2041e0cd
NjauSamuel <njaus602@gmail.com>
2024-09-04 18:57:37 +0300

    Implemented box, mobile first designs for my UI with an extractable box component stored in the components file.

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,25 +1,28 @@
 <template>
-    <div v-for="listing in listings" :key="listing.id">
-        <div>
-            <Link :href="route('listing.show', {listing: listing.id})">
-                {{ listing.street }} - {{ listing.street_nr }} - {{ listing.city }}, for {{ listing.price }}
-            </Link>
-        </div>
-    
-        <div>
-            <Link :href="route('listing.edit', listing.id)">Edit</Link>
-        </div>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <Box v-for="listing in listings" :key="listing.id">
+            <div>
+                <Link :href="route('listing.show', {listing: listing.id})">
+                    {{ listing.street }} - {{ listing.street_nr }} - {{ listing.city }}, for {{ listing.price }}
+                </Link>
+            </div>
+        
+            <div>
+                <Link :href="route('listing.edit', listing.id)">Edit</Link>
+            </div>
 
-        <div>
-            <Link :href="route('listing.destroy', listing.id)" method="Delete" as="button">Delete</Link>
-        </div>
+            <div>
+                <Link :href="route('listing.destroy', listing.id)" method="Delete" as="button">Delete</Link>
+            </div>
 
+        </Box>
     </div>
 </template>
 
 <script setup>
 
     import {Link} from '@inertiajs/vue3'
+    import Box from '@/Components/UI/Box.vue'
 
     defineProps({
         listings: Array, 
