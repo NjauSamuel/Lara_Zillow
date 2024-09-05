89cbd4c5220210dc96a29376178b819594976202
NjauSamuel <njaus602@gmail.com>
2024-09-05 18:15:37 +0300

    Did the markup for the Mortgage calculator component.

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -18,9 +18,27 @@
 
             <Box>
                 <template #header>
-                    Offer
+                    Monthly Payment
                 </template>
-                Make an offer
+                <div>
+                    <label>Interest Rate (2.5%)</label>
+                    <input 
+                        type="range" min="0.1" max="30" step="0.1" 
+                        class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                    />
+                    
+                    <label>Duration (25 years)</label>
+                    <input 
+                        type="range" min="1" max="35" step="1" 
+                        class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                    />
+
+                    <div class="text-gray-600 dark:text-gray-300 mt-2">
+                        <div class="text-gray-400">Your Monthly Payment</div>
+                        <Price :price="500" class="text-3-xl" />
+                    </div>
+                    
+                </div>
             </Box>
         </div>
     </div>
