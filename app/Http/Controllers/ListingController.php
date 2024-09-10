6f449f594be16aa949eff0e22702788f8560cdae
NjauSamuel <njaus602@gmail.com>
2024-09-10 18:25:12 +0300

    Implemented pagination for the site.

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -14,7 +14,7 @@ class ListingController extends Controller
     public function index()
     {
         return inertia('Listing/Index', [
-            'listings' => Listing::all()
+            'listings' => Listing::orderBy('created_at', 'desc')->paginate(10)
         ]);
     }
 
