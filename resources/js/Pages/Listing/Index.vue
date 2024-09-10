954b9b008731d3cd8c58b2f4f525be3d8d27faa2
NjauSamuel <njaus602@gmail.com>
2024-09-10 19:36:36 +0300

    Implemented a filter form on the home page of the site.

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,11 +1,14 @@
 <template>
+
+    <Filters />
+
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
 
         <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />
 
     </div>
 
-    <div v-if="listings.data.length" class="w-full flex justify-center my-8">
+    <div v-if="listings.data.length" class="w-full flex justify-center my-4">
       <Pagination :links="listings.links"></Pagination>
     </div>
   </template>
@@ -13,7 +16,8 @@
   <script setup>
 
     import Listing from '@/Pages/Listing/Index/Components/Listing.vue';
-import Pagination from '../../Components/UI/Pagination.vue';
+    import Pagination from '@/Components/UI/Pagination.vue';
+    import Filters from '@/Pages/Listing/Index/Components/Filters.vue';
 
     defineProps({
         listings: Object, 
