267c6a302466922c2f66715266bf14582d9224c1
NjauSamuel <njaus602@gmail.com>
2024-09-19 21:57:13 +0300

    Made the file upload form submit through vue forms instead of traditional Laravel forms.

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,15 +1,40 @@
 <template>
-    <form
-        enctype="multipart/form-data" 
-        method="Post" :action="route('realtor.listing.image.store', {listing: listing.id})">
-        <input type="file" multiple name="images[]" />
 
-        <button type="submit">Send</button>
-    </form>
+    <Box>
+        <template #header>Upload New Images</template>
+        <form @submit.prevent="upload">
+            <input type="file" multiple @input="addFiles" />
+
+            <button type="submit" class="btn-outline">Upload</button>
+            <button type="reset" @click="reset" class="btn-outline">Reset</button>
+        </form>
+    </Box>
+
 </template>
 
 <script setup>
-    defineProps({
-        listing: Object
-    })
+    import Box from '@/Components/UI/Box.vue'
+import { useForm } from '@inertiajs/vue3';
+
+    const props = defineProps({ listing: Object })
+
+    const form = useForm({images: []})
+
+    const upload = () => {
+        form.post(
+            route('realtor.listing.image.store', {listing: props.listing.id}), 
+            {
+                onSuccess: () => form.reset('images'),
+            }
+        )
+    }
+
+    const addFiles = (event) => {
+        for (const image of event.target.files){
+            form.images.push(image)
+        }
+    }
+
+    const reset = () => form.reset('images')
+    
 </script>
