91faec7287acee5a669025b8a49893f6e625c747
NjauSamuel <njaus602@gmail.com>
2024-09-19 21:24:02 +0300

    Removed the unnecessary index controller that was only for demonstration purposes.

--- resources/js/Pages/Index/Show.vue
+++
@@ -1,9 +0,0 @@
-<template>
-
-  <div> Show </div>
-
-</template>
-
-<script setup>
-  import MainLayout from '../../Layouts/MainLayout.vue';
-</script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\RealtorListingController;
 use App\Http\Controllers\RealtorListingImageController;
@@ -10,9 +9,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [ListingController::class, 'index']);
-
-Route::get('/hello', [IndexController::class, 'show'])
-    ->middleware('auth');
 
 Route::resource('listing', ListingController::class)
     ->only(['index', 'show']);
