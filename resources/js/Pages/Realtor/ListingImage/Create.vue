43745bf1d0f958983f671f54b75164eb68e151f6
NjauSamuel <njaus602@gmail.com>
2024-09-19 22:13:23 +0300

    Styled the upload form better.

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,12 +1,14 @@
 <template>
 
     <Box>
-        <template #header>Upload New Images</template>
+        <template #header >Upload New Images</template>
         <form @submit.prevent="upload">
-            <input type="file" multiple @input="addFiles" />
+            <section class="flex items-center gap-2 my-4">
+                <input type="file" multiple @input="addFiles" class="border rounded-md file:px-4 file:py-2 border-gray-400 dark:border-gray-700 file:text-gray-700 file:dark:text-gray-400 file:border-0 file:bg-gray-100 file:dark:bg-gray-800 file:font-medium file:hover:bg-gray-200 file:dark:hover:bg-gray-700 file:hover:cursor-pointer file:mr-4" />
 
-            <button type="submit" class="btn-outline">Upload</button>
-            <button type="reset" @click="reset" class="btn-outline">Reset</button>
+                <button type="submit" class="btn-outline disabled:opacity-25 disabled:cursor-not-allowed" :disabled="!canUpload">Upload</button>
+                <button type="reset" @click="reset" class="btn-outline">Reset</button>
+            </section>
         </form>
     </Box>
 
@@ -14,7 +16,8 @@
 
 <script setup>
     import Box from '@/Components/UI/Box.vue'
-import { useForm } from '@inertiajs/vue3';
+    import { useForm } from '@inertiajs/vue3';
+    import {computed} from 'vue'
 
     const props = defineProps({ listing: Object })
 
@@ -36,5 +39,7 @@ import { useForm } from '@inertiajs/vue3';
     }
 
     const reset = () => form.reset('images')
+
+    const canUpload = computed(() => form.images.length)
     
 </script>
