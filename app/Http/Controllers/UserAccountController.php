ae3d3bd14757b1aa6a15a3213e69934d1846c6b3
NjauSamuel <njaus602@gmail.com>
2024-10-14 09:46:03 +0300

    Started the email verification process.

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -24,6 +25,9 @@ class UserAccountController extends Controller
         );
 
         Auth::login($user);
+
+        // Creating the Event for email verification
+        event(new Registered($user));
 
         return redirect()->route('listing.index')
             ->with('success', 'Account Created!');
